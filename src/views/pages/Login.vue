85a85b1a90ecf05420853e7891c6fe9cf22da2a1
MSI\laptop msi <lauraq2348@gmail.com>
2022-10-01 12:15:09 -0400

    cambios en el login y su redirect

--- src/views/pages/Login.vue
+++ src/views/pages/Login.vue
@@ -4,19 +4,19 @@
       <CRow class="justify-content-center">
         <CCol :md="8">
           <CCardGroup>
+            <CCard class="text-white py-5" style="width: 44%">
+              <CCardBody src=""></CCardBody>
+            </CCard>
             <CCard class="p-4">
               <CCardBody>
                 <CForm>
-                  <h1>Login</h1>
-                  <p class="text-medium-emphasis">Sign In to your account</p>
+                  <h1>Iniciar Sesion</h1>
+                  <p class="text-medium-emphasis"></p>
                   <CInputGroup class="mb-3">
                     <CInputGroupText>
                       <CIcon icon="cil-user" />
                     </CInputGroupText>
-                    <CFormInput
-                      placeholder="Username"
-                      autocomplete="username"
-                    />
+                    <CFormInput placeholder="Usuario" autocomplete="Correo" />
                   </CInputGroup>
                   <CInputGroup class="mb-4">
                     <CInputGroupText>
@@ -24,36 +24,21 @@
                     </CInputGroupText>
                     <CFormInput
                       type="password"
-                      placeholder="Password"
+                      placeholder="Contraseña"
                       autocomplete="current-password"
                     />
                   </CInputGroup>
                   <CRow>
                     <CCol :xs="6">
-                      <CButton color="primary" class="px-4"> Login </CButton>
+                      <CButton color="primary" class="px-4"> Ingresar </CButton>
                     </CCol>
                     <CCol :xs="6" class="text-right">
                       <CButton color="link" class="px-0">
-                        Forgot password?
+                        ¿Olvidaste tu Contraseña?
                       </CButton>
                     </CCol>
                   </CRow>
                 </CForm>
-              </CCardBody>
-            </CCard>
-            <CCard class="text-white bg-primary py-5" style="width: 44%">
-              <CCardBody class="text-center">
-                <div>
-                  <h2>Sign up</h2>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua.
-                  </p>
-                  <CButton color="light" variant="outline" class="mt-3">
-                    Register Now!
-                  </CButton>
-                </div>
               </CCardBody>
             </CCard>
           </CCardGroup>
