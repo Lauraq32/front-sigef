9329aa8bcdc63e7e87a022333a49668b34e1b6fe
edwar <edwardgabriel0700@gmail.com>
2022-10-02 13:26:58 -0400

    Iconos agregados a los modulos de Financiero,Compras,RRHH y administrativo. Icono de la pagina cambiado al logo del sigef

--- src/views/Administrativo.vue
+++ src/views/Administrativo.vue
@@ -1,27 +1,290 @@
 <template lang="">
-  <h1>Administrativo</h1>
   <div>
     <CRow>
-      <CCol :md="3">
-        <CCard style="width: 18rem" class="mt-4">
-          <CCardImage orientation="top" src="/images/vue.jpg" />
-          <CCardBody>
-            <CCardTitle>Card title</CCardTitle>
-            <CCardText
-              >Some quick example text to build on the card title and make up
-              the bulk of the card's content.</CCardText
-            >
-            <CButton href="#">Go somewhere</CButton>
-          </CCardBody>
-        </CCard>
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Manejo Usuarios', value: '' }]"
+        >
+          <template #icon>
+            <img
+              class="card-img-top"
+              src="../assets/images/RegistroPersonal.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
       </CCol>
-   
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Transportación', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/Transportacion.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
+      </CCol>
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Visitas', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/Visitas.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
+      </CCol>
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Viáticos', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/Viatico.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
+      </CCol>
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Caja chica', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/CajaChica.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
+      </CCol>
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Combustibles', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/Combustible.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
+      </CCol>
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Digitalización', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/Digitalizacion.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
+      </CCol>
+     
     </CRow>
   </div>
 </template>
 <script>
+import prueba from '../assets/images/logo.png'
+
 export default {
-  name: 'Administrativo',
+  name: 'Fianciero',
 }
 </script>
 <style lang=""></style>
--- src/views/Compras.vue
+++ src/views/Compras.vue
@@ -1,27 +1,94 @@
 <template lang="">
   <div>
-    <h1>Compras</h1>
     <CRow>
-      <CCol :md="3">
-        <CCard style="width: 18rem" class="mt-4">
-          <CCardImage orientation="top" src="/images/vue.jpg" />
-          <CCardBody>
-            <CCardTitle>Card title</CCardTitle>
-            <CCardText
-              >Some quick example text to build on the card title and make up
-              the bulk of the card's content.</CCardText
-            >
-            <CButton href="#">Go somewhere</CButton>
-          </CCardBody>
-        </CCard>
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Plan anual de compras', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/Compra.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
       </CCol>
-   
+      <CCol :md="3" class="mt-4">
+        <CWidgetStatsD
+          class="mb-4 shadow"
+          style="--cui-card-cap-bg: #3b5998"
+          :values="[{ title: 'Proceso de compras', value: '' }]"
+        >
+          <template #icon>
+            <img class="card-img-top" src="../assets/images/Proceso.png"
+          /></template>
+          <template #chart>
+            <CChart
+              class="position-absolute w-100 h-100"
+              type="line"
+              :data="{
+                labels: [
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: 'rgba(255,255,255,.1)',
+                    borderColor: 'rgba(255,255,255,.55)',
+                    pointHoverBackgroundColor: '#fff',
+                    borderWidth: 2,
+                    data: [65, 59, 84, 84, 51, 55, 40],
+                    fill: true,
+                  },
+                ],
+              }"
+              :options="options"
+            />
+          </template>
+        </CWidgetStatsD>
+      </CCol>
+     
+     
     </CRow>
   </div>
 </template>
 <script>
+import prueba from '../assets/images/logo.png'
+
 export default {
-  name: 'Compras',
+  name: 'Fianciero',
 }
 </script>
 <style lang=""></style>
