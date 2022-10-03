9a8cf65c478b2f97c0ccbca3ff16a69990e8392a
LIGAMD\rcarmona <raymondcarmonatejeda01@gmail.com>
2022-10-03 10:28:29 -0400

    reportes Clasificadores

--- src/modules/financiero/FormulacionModule/components/Clasificadores.vue
+++ src/modules/financiero/FormulacionModule/components/Clasificadores.vue
@@ -1,7 +1,12 @@
 <template>
    <p>Clasificadores</p>
   <div>
-    <CButton color="info">Imprimir</CButton>
+    <div class="d-inline p-2">
+      <CButton color="info " @click="IngresoReport">Imprimir Clasificadores de Ingresos</CButton>
+    </div>
+    <div class="d-inline p-2">
+      <CButton color="info" @click="GastoReport">Imprimir Clasificadores de Gastos</CButton>
+    </div>
   </div>
   <hr />
   <CTable responsive>
@@ -55,3 +60,16 @@
     </CTableBody>
   </CTable>
 </template>
+
+<script>
+  export default{
+    methods:{
+    IngresoReport() {
+      window.open(`http://server-iis/ReportServer/Pages/ReportViewer.aspx?%2fReporte_FP%2fRep_Clasificadores_Ingreso&rs:Command=Render`, '_blank').focus();
+    },
+    GastoReport() {
+      window.open(`http://server-iis/ReportServer/Pages/ReportViewer.aspx?%2fReporte_FP%2fRep_Clasificadores_Gasto&rs:Command=Render`, '_blank').focus();
+    },
+  }
+  }
+</script>
