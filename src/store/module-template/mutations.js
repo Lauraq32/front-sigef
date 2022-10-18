4291dd7acf143ef048e1413513e42834dbcda9cc
AlfredoRD <61297115+AlfredoRD@users.noreply.github.com>
2022-10-18 11:52:27 -0400

    ejemplos del store/editar y eliminar

--- src/store/module-template/mutations.js
+++ src/store/module-template/mutations.js
@@ -1,15 +1,35 @@
+/* eslint-disable prettier/prettier */
+
+
+
 import router from '@/router'
 import Api from '../services/Auth'
 
-export const SET_USER = (state,user) => {
-    Api.Login(user.email,user.password).then(response => {
+export const SET_USER = (state, user) => {
+    Api.Login(user.email, user.password).then(response => {
         state.user = response.data
-        localStorage.setItem('usuario',response.data.nombre)
-        localStorage.setItem('nombre_ayuntamiento',response.data.ayuntamiento)
-        localStorage.setItem('ano',response.data.anofiscal)
-        localStorage.setItem('id_Ayuntamiento',response.data.id_Ayuntamiento)
-    
+        localStorage.setItem('usuario', response.data.nombre)
+        localStorage.setItem('nombre_ayuntamiento', response.data.ayuntamiento)
+        localStorage.setItem('ano', response.data.anofiscal)
+        localStorage.setItem('id_Ayuntamiento', response.data.id_Ayuntamiento)
+
     })
-        router.push({ name: 'financiero' })
-      
+    router.push({ name: 'financiero' })
+
 }
+
+
+//actualizar entradas
+export const updateEntries = (state, entry) => {
+
+    const idx = state.entries.map(e => e.id).indexOf(entry.id)
+    state.entries[idx] = entry
+
+}
+
+//Borrar entrada
+export const deleteEntry = (state, id) => {
+    state.entries = state.entries.filter(entry => entry.id !== id)
+
+}
+
