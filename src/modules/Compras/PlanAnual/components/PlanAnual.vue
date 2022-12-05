a7966ad53a843191cc2764810b7f90c8e11ca843
LIGAMD\EFrias <20197657@itla.edu.do>
2022-12-05 13:28:26 -0400

    Compras casi listo, añadi algunos procesos de compras tipo ejemplos

--- src/modules/Compras/PlanAnual/components/PlanAnual.vue
+++ src/modules/Compras/PlanAnual/components/PlanAnual.vue
@@ -1,54 +1,97 @@
 <template>
-  <h1>Plan anual de compras</h1>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col"></th>
-        <th scope="col">Plan anual de compras 2023</th>
-        <th scope="col">Monto</th>
-        <th scope="col"> Descargar</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>PLAN ANUAL COMPRAS DIRECCIÓN TECNOLOGÍA</td>
-        <td>5,000,000.00$ DOP</td>
-        <td>
+  <h1 style="text-align:center">Plan anual de compras</h1>
+  <hr>
+  <div class="caja">
+    <ol>
+      <li style="list-style: none" @click="visible = !visible" aria-current="true">
+        <h5 style="
+            background-color: rgb(223 223 225 / 68%);
+            
+            padding: 15px;
+            border-radius: 20px;
+          ">
+          Plan anual de compras liga municipal dominicana(LMD) 2023
+        </h5>
+      </li>
+    </ol>
 
-          <a target="blanck"
-            href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
-            Descagar </a>
+    <CCollapse class="mt-3 mb-3" :visible="visible">
+      <CCard class="mt-3">
+        <CCardBody>
+          <ol style="">
+            <div class="d-flex justify-content-between mt-3">
+              <li>
+                <a target="blanck"
+                  href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
+                  Proceso de compras 1 </a>
 
-        </td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>PLAN ANUAL COMPRAS JUVENTUD</td>
-        <td>2,300,00.00$ DOP</td>
-        <td>
+                 
 
-          <a target="blanck"
-            href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
-            Descagar </a>
 
-        </td>
+              </li>
+              <span>50,000.00RD$</span>
 
-      </tr>
 
-      <tr>
-        <th scope="row">3</th>
-        <td>PLAN ANUAL COMPRAS JUVENTUD</td>
-        <td>2,300,00.00$ DOP</td>
-        <td>
+              <CIcon icon="cisCheckCircle" size="lg" />
+            </div>
+            <div class="d-flex justify-content-between mt-3">
+              <li>
+                <a target="blanck"
+                  href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
+                  Proceso de compras 2</a>
+              </li>
 
-          <a target="blanck"
-            href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
-            Descagar </a>
+              <CIcon icon="cisCheckCircle" size="lg" />
+            </div>
 
-        </td>
-      </tr>
+            <div class="d-flex justify-content-between mt-3">
+              <li>
+                <a target="blanck"
+                  href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
+                  Proceso de compras 3</a>
+              </li>
 
-    </tbody>
-  </table>
+              <CIcon icon="cisCheckCircle" size="lg" />
+            </div>
+            <div class="d-flex justify-content-between mt-3">
+              <li>
+                <a target="blanck"
+                  href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
+                  Proceso de compras 4</a>
+              </li>
+
+              <CIcon icon="cisCheckCircle" size="lg" />
+            </div>
+          </ol>
+        </CCardBody>
+      </CCard>
+    </CCollapse>
+
+
+  </div>
 </template>
+<script>
+import { CIcon } from '@coreui/icons-vue'
+import * as icon from '@coreui/icons'
+export default {
+  data() {
+    return {
+      icon,
+      CIcon,
+      visible: false,
+      visible1: false,
+      visible2: false,
+      visible3: false,
+      visible4: false,
+    }
+  },
+}
+</script>
+
+<style>
+a {
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+}
+</style>
