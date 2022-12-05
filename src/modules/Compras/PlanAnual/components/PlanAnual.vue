43fd41547a4c036c06ca923cdb6b4cf15eb159a5
LIGAMD\EFrias <20197657@itla.edu.do>
2022-12-05 10:53:49 -0400

    Plan anual de compras realizado, faltan cosas por reajustar.

--- src/modules/Compras/PlanAnual/components/PlanAnual.vue
+++ src/modules/Compras/PlanAnual/components/PlanAnual.vue
@@ -1,3 +1,54 @@
 <template>
-  <h1>klk</h1>
+  <h1>Plan anual de compras</h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col"></th>
+        <th scope="col">Plan anual de compras 2023</th>
+        <th scope="col">Monto</th>
+        <th scope="col"> Descargar</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>PLAN ANUAL COMPRAS DIRECCIÓN TECNOLOGÍA</td>
+        <td>5,000,000.00$ DOP</td>
+        <td>
+
+          <a target="blanck"
+            href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
+            Descagar </a>
+
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>PLAN ANUAL COMPRAS JUVENTUD</td>
+        <td>2,300,00.00$ DOP</td>
+        <td>
+
+          <a target="blanck"
+            href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
+            Descagar </a>
+
+        </td>
+
+      </tr>
+
+      <tr>
+        <th scope="row">3</th>
+        <td>PLAN ANUAL COMPRAS JUVENTUD</td>
+        <td>2,300,00.00$ DOP</td>
+        <td>
+
+          <a target="blanck"
+            href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwidl4-sj9n7AhWWmWoFHSE_Di8QFnoECAoQAQ&url=https%3A%2F%2Fwww.digepres.gob.do%2Fwp-content%2Fuploads%2F2019%2F08%2FLey-87-01-que-crea-el-Sistema-Nacional-de-Seguridad-Social.pdf&usg=AOvVaw3emafIdFoY1w09QQ3UwKir">
+            Descagar </a>
+
+        </td>
+      </tr>
+
+    </tbody>
+  </table>
 </template>
