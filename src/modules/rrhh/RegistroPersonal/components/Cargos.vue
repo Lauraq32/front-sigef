5d17809f666f09d7b5570b3fefd927af3d841d85
Lauraq32 <lauraq2348@gmail.com>
2023-03-28 08:49:53 -0400

    fix: modal, table

--- src/modules/rrhh/RegistroPersonal/components/Cargos.vue
+++ src/modules/rrhh/RegistroPersonal/components/Cargos.vue
@@ -53,7 +53,7 @@
           size="sm"
           @click="toggleDetails(item, index)"
         >
-          {{ Boolean(item._toggled) ? 'Hide' : 'Show' }}
+          {{ Boolean(item._toggled) ? 'Hide' : 'Eliminar' }}
         </CButton>
       </td>
     </template>
@@ -71,7 +71,7 @@
     </template>
   </CSmartTable>
   <CModal
-    size="lg"
+    size="md"
     :visible="lgDemo"
     @close="
       () => {
@@ -80,7 +80,7 @@
     "
   >
     <CModalHeader>
-      <CModalTitle>Posiciones o cargos</CModalTitle>
+      <CModalTitle>Cargos</CModalTitle>
     </CModalHeader>
     <CModalBody>
       <CCardBody>
@@ -90,13 +90,7 @@
           :validated="validatedCustom01"
           @submit="handleSubmitCustom01"
         >
-          <CCol :md="2">
-            <CFormLabel for="validationCustom01">Código</CFormLabel>
-            <CFormInput disabled id="validationCustom04"> </CFormInput>
-            <CFormFeedback valid> Exito! </CFormFeedback>
-            <CFormFeedback invalid> Favor agregar el campo </CFormFeedback>
-          </CCol>
-          <CCol :md="2">
+          <CCol :md="12">
             <CFormLabel for="validationCustomUsername"
               >Posicion o cargo</CFormLabel
             >
@@ -109,8 +103,9 @@
               type="button"
               class="btn btn-secondary"
               data-bs-dismiss="modal"
+              v-on:click="close"
             >
-              Close
+              Cerrar
             </button>
             <button class="btn btn-info btn-block mt-1" v-on:click="Guardar">
               Guardar
@@ -134,7 +129,6 @@ export default {
       validatedCustom01: null,
       lgDemo: false,
       columns: [
-        { key: 'Código', label: 'Código', _style: { width: '40%' } },
         {
           key: 'Posicion o cargo',
           label: 'Posicion o cargo',
@@ -153,6 +147,9 @@ export default {
     }
   },
   methods: {
+    close() {
+      this.lgDemo = false
+    },
     handleSubmitCustom01(event) {
       const form = event.currentTarget
       if (form.checkValidity() === false) {
