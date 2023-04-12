bde5fe072b7698c5e45a20e9a77f5a5d9ee66f44
Geraldson <Geraldson.a.p@gmail.com>
2023-04-12 16:00:08 -0400

    Adds auto logout timer

---
+++ src/components/SessionManager.vue
@@ -0,0 +1,40 @@
+<template >
+  <v-idle @idle="handleIdle" :duration="300" :loop="false" />
+</template>
+
+<script>
+  import Swal from 'sweetalert2/dist/sweetalert2.js'
+  import {useAuthStore} from "@/store/AuthStore"
+  import { mapActions } from 'pinia'
+  import router from '@/router/index'
+  export default {
+    name:"SessionManager",
+    methods: {
+      ...mapActions(useAuthStore, ["signOut"]),
+
+      handleIdle(){
+
+        this.signOut().then(() => {
+
+        Swal.fire({
+          position: 'center',
+          icon: 'info',
+          title: 'Su sesión ha expirado',
+          showConfirmButton: true,
+          allowEscapeKey:false,
+          allowOutsideClick:false
+        }).then(() => {
+
+          router.push({name:"Login", replace:true})
+        })})
+      }
+    }
+  }
+
+</script>
+
+<style>
+  .v-idle{
+    visibility: hidden;
+  }
+</style>
