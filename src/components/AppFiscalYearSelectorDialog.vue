a5ea1c7f7d5ebf7ac76886c60d8d8227679fe5cb
Johanny S. Novas <johanny.novas@lmd@gob.do>
2023-04-18 11:06:28 -0400

    CG-23: adding search filter

--- src/components/AppFiscalYearSelectorDialog.vue
+++ src/components/AppFiscalYearSelectorDialog.vue
@@ -12,15 +12,35 @@
             </CModalTitle>
         </CModalHeader>
         <CModalBody>
-            <div class="container-fluid d-flex flex-wrap justify-content-center">
+            <header class="d-flex justify-content-center">
+                <div class="position-relative mb-4 w-50">
+                    <input
+                        @input="filterFiscalYearByAnioIdStatus"
+                        class="form-control padding-input"
+                        placeholder="Filtrar por Id, año, estatus, si es aprobrado"
+                    />
+                    <span class="position-absolute icon-input">
+                        <CIcon icon="cisSearch" size="xl" />
+                    </span>
+                </div>
+            </header>
+            <div v-if="fiscalYearListToShow.length === 0"
+                class="container-fluid d-flex flex-wrap justify-content-center">
+                <span class="h4">Sin resultados</span>
+            </div>
+            <div v-else
+                class="container-fluid d-flex flex-wrap justify-content-center">
                 <section
-                    v-for="fiscarYear of fiscalYearList"
+                    v-for="fiscarYear of fiscalYearListToShow"
                     :key="fiscarYear.id"
-                    class="fy-card d-flex flex-column m-2 border border-primary shadow-sm p-3"
+                    class="fy-card d-flex flex-column m-2 border border-primary shadow-sm p-3 position-relative"
                     @click="closeDialog(fiscarYear)"
                 >
                     <header>
                         <h3 class="display-3">{{ fiscarYear.anio }}</h3>
+                        <CBadge color="primary" position="top-start" shape="rounded-pill">
+                            {{ fiscarYear.id }}
+                        </CBadge>
                     </header>
                     <small
                         class="upper-case"
@@ -39,6 +59,7 @@
 </template>
 <script setup>
 import { CModal, CModalBody, CModalHeader, CModalTitle } from '@coreui/vue';
+import { ref, watchEffect } from 'vue';
 
 const emits = defineEmits(['select']);
 const props = defineProps({
@@ -57,6 +78,24 @@ const props = defineProps({
 const closeDialog = (fiscarYear) => {
     emits('select', fiscarYear);
 }
+const fiscalYearListToShow = ref([]);
+const filterFiscalYearByAnioIdStatus = ({target: {value}}) => {
+    if (value === '') {
+        fiscalYearListToShow.value = [...props.fiscalYearList];
+        return;
+    }
+
+    fiscalYearListToShow.value = props.fiscalYearList.filter(fy => {
+        return [`${fy.id}`, `${fy.anio}`, fy.esAprobado ? 'Aprobado' : ''].join(' ').includes(value) 
+    });
+}
+
+watchEffect(() => {
+    if (props.fiscalYearList.length) {
+        fiscalYearListToShow.value = [...props.fiscalYearList];
+    }
+});
+
 </script>
 <style scoped>
 .upper-case {
@@ -70,5 +109,16 @@ const closeDialog = (fiscarYear) => {
 .fy-card:hover {
     transform: scale(1.1, 1.1);
     background-color: #fff;
+}
+.padding-input {
+    padding-right: 2.5rem;
+}
+
+.icon-input {
+    padding: 0.2rem;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+    right: 2px;
 }
 </style>
