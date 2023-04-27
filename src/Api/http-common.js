4c25de1735ad8b2e0f332af9ec4e16e5e852295a
Johanny S. Novas <johanny.novas@lmd@gob.do>
2023-04-27 19:26:48 -0400

    BLD-02: removing lint is not working

--- src/Api/http-common.js
+++ src/Api/http-common.js
@@ -1,34 +1,32 @@
 import axios from 'axios';
 import { getFiscalYearId, getLoggedInfo } from '@/utils/logged-info';
-import router from '../router'
-
+import router from '../router';
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.APP_API_URL}/api`,
-  headers: {
-    'Content-type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'DELETE, PUT, POST, GET, OPTIONS',
-    'Accept-Language': 'es-ES',
-    'Content-Language': 'es-ES',
-  },
+	baseURL: `${process.env.VUE_APP_API_URL}/api`,
+	headers: {
+		'Content-type': 'application/json',
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'DELETE, PUT, POST, GET, OPTIONS',
+		'Accept-Language': 'es-ES',
+		'Content-Language': 'es-ES',
+	},
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  config.headers['Authorization'] = `ApiKey ${getLoggedInfo()?.token}`;
-  config.headers['X-Fiscal-Year-Id'] =  getFiscalYearId();
-  return config;
+	config.headers['Authorization'] = `ApiKey ${getLoggedInfo()?.token}`;
+	config.headers['X-Fiscal-Year-Id'] = getFiscalYearId();
+	return config;
 });
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error?.response?.status === 401) {
-      router.push({ name: "Login", replace: true})
-      .catch(console.error);
-    }
-    return Promise.reject(error);
-  }
+	(response) => response,
+	(error) => {
+		if (error?.response?.status === 401) {
+			router.push({ name: 'Login', replace: true }).catch(console.error);
+		}
+		return Promise.reject(error);
+	},
 );
 
 export default axiosInstance;
