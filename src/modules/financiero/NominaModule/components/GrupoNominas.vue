c9ebd6245157fd365ff11045f3daf7e55571fe2b
LIGAMD\avalenzuela <alfredoelian214@gmail.com>
2023-06-21 11:17:37 -0400

    fix

--- src/modules/financiero/NominaModule/components/GrupoNominas.vue
+++ src/modules/financiero/NominaModule/components/GrupoNominas.vue
@@ -1,5 +1,5 @@
 <template>
-  <h3 class="text-center">Grupos de Nómina</h3>
+  <h3 class="text-center">Grupos Nómina</h3>
   <div class="table-headers">
     <div class="d-inline p-2">
       <CButton
