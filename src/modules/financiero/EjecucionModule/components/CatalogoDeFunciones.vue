1782b69faf10a70f33d004151346225c72ce25ce
LIGAMD\avalenzuela <alfredoelian214@gmail.com>
2023-07-07 14:22:17 -0400

    fix

--- src/modules/financiero/EjecucionModule/components/CatalogoDeFunciones.vue
+++ src/modules/financiero/EjecucionModule/components/CatalogoDeFunciones.vue
@@ -6,7 +6,7 @@
         color="info"
         @click="
           () => {
-            grupoModal = true
+            showModalCatalogoFuncion = true
           }
         "
         >Agregar</CButton
@@ -34,33 +34,89 @@
     :sorterValue="{ column: 'status', state: 'asc' }"
     pagination
   >
+    <template #grupo="{ item }">
+      <td class="text-center">
+        {{ item.grupo }}
+      </td>
+    </template>
+    <template #funcion="{ item }">
+      <td class="text-center">
+        {{ item.funcion }}
+      </td>
+    </template>
+    <template #sGrupo="{ item }">
+      <td class="text-center">
+        {{ item.sGrupo }}
+      </td>
+    </template>
     <template #show_details="{ item }">
-      <td class="py-2">
-        <CButton
-          color="primary"
-          variant="outline"
-          square
-          size="sm"
-          @click="editarGrupo(item)"
-        >
-          Editar
-        </CButton>
+      <td>
+        <CDropdown>
+          <CDropdownToggle color="primary" variant="outline"
+            >Acciones</CDropdownToggle
+          >
+          <CDropdownMenu>
+            <CDropdownItem @click="() => handlerAccion(item)"
+              >Eliminar</CDropdownItem
+            >
+            <CDropdownItem @click="() => handlerAccion(item)"
+              >Editar</CDropdownItem
+            >
+          </CDropdownMenu>
+        </CDropdown>
       </td>
     </template>
   </CSmartTable>
+
+  <ModalCatalogoFunciones
+    :showModal="showModalCatalogoFuncion"
+    @close-modal="closeCatalogoFuncion"
+  />
 </template>
 
 <script>
 import Api from '../services/EjecucionServices'
 import { CSmartTable } from '@coreui/vue-pro'
+import ModalCatalogoFunciones from './Dialogos/CatalogoDeFuncionesModal.vue'
 export default {
   name: 'catalogoDeFunciones',
   components: {
     CSmartTable,
+    ModalCatalogoFunciones,
   },
   data: () => {
     return {
+      showModalCatalogoFuncion: false,
       catalogoFunciones: [],
+      columns: [
+        {
+          label: 'Finalidad',
+          key: 'grupo',
+          _style: { width: '19%' },
+        },
+        {
+          label: 'Función',
+          key: 'funcion',
+          _style: { width: '20%' },
+        },
+        {
+          label: 'Sub-Función',
+          key: 'sGrupo',
+          _style: { width: '20%' },
+        },
+        {
+          label: 'Denominación',
+          key: 'detalle',
+          _style: { width: '20%' },
+        },
+        {
+          key: 'show_details',
+          label: '',
+          _style: { width: '1%' },
+          filter: false,
+          sorter: false,
+        },
+      ],
     }
   },
 
@@ -69,6 +125,10 @@ export default {
       Api.getCatalogoFunciones().then((response) => {
         this.catalogoFunciones = response.data.data
       })
+    },
+
+    closeCatalogoFuncion() {
+      this.showModalCatalogoFuncion = false
     },
   },
 
---
+++ src/modules/financiero/EjecucionModule/components/Dialogos/CatalogoDeFuncionesModal.vue
@@ -0,0 +1,90 @@
+<template>
+  <CModal
+    :visible="showModal"
+    @close="closeCatalogoFuncion"
+    backdrop="static"
+  >
+    <CModalHeader>
+      <CModalTitle>Catalogo de Funciones</CModalTitle>
+    </CModalHeader>
+    <CModalBody>
+      <CCardBody>
+        <CForm
+          class="needs-validation"
+          novalidate
+          :validated="grupoFormValidated"
+          ref="formRef"
+        >
+          <CCol>
+            <CFormLabel for="postGrupo.descripcion">Finalidad</CFormLabel>
+            <CFormInput id="postGrupo.descripcion" required />
+            <CFormFeedback invalid> Favor agregar el campo </CFormFeedback>
+          </CCol>
+          <CCol>
+            <CFormLabel for="postGrupo.fechaInicio">Funci&oacute;n</CFormLabel>
+            <CFormInput id="postGrupo.fechaInicio" required />
+            <CFormFeedback invalid> Favor agregar el campo </CFormFeedback>
+          </CCol>
+          <CCol>
+            <CFormLabel for="postGrupo.fechaFin">Sub-Funci&oacute;n</CFormLabel>
+            <CFormInput id="postGrupo.fechaFin" required />
+            <CFormFeedback invalid>
+              La fecha no puede ser menor a la fecha inicio
+            </CFormFeedback>
+          </CCol>
+          <CCol>
+            <CFormLabel for="postGrupo.fechaInicio"
+              >Denominaci&acute;</CFormLabel
+            >
+            <CFormInput id="postGrupo.fechaInicio" required />
+            <CFormFeedback invalid> Favor agregar el campo </CFormFeedback>
+          </CCol>
+        </CForm>
+      </CCardBody>
+    </CModalBody>
+    <CModalFooter>
+      <CButton
+        type="button"
+        color="secondary"
+        data-bs-dismiss="modal"
+        @click="closeCatalogoFuncion"
+        >Cerrar
+      </CButton>
+      <CButton type="button" color="primary" v-on:click="saveGrupo">
+        Guardar
+      </CButton>
+    </CModalFooter>
+  </CModal>
+</template>
+<script>
+import { CModal } from '@coreui/vue'
+
+export default {
+  name: 'catalogoFuncionesModal',
+  components: {
+    CModal,
+  },
+
+  data: function () {
+    return {
+      grupoFormValidated: false,
+      postCatalogoDeFunciones: {
+        descripcion: '',
+        fechaInicio: new Date(),
+        fechaFin: new Date(),
+      },
+    }
+  },
+
+  methods: {
+    closeCatalogoFuncion() {
+      this.$emit('close-modal', false)
+      this.clearForm()
+    },
+  },
+
+  props: {
+    showModal: Boolean,
+  },
+}
+</script>
