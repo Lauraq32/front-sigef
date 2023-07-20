1b54c7b486864f302e754f723146ee38a6c81b0e
LIGAMD\rcarmona <randolph.carmona@lmd.gob.do>
2023-07-20 14:05:48 -0400

    EP-9 quick fixes

--- src/modules/financiero/EjecucionModule/components/dialogs/CompranteGastoCapturaDialog.vue
+++ src/modules/financiero/EjecucionModule/components/dialogs/CompranteGastoCapturaDialog.vue
@@ -18,13 +18,13 @@
                       </CCol>
                       <CCol :md="6">
                         <CFormLabel for="nombre">Comprobante Modificado</CFormLabel>
-                        <CFormInput @keypress="onlyNumber" id="nombre" required />
+                        <CFormInput @keypress="onlyNumber" id="nombre"  />
                       </CCol>
 
 
                       <CCol :md="6">
                         <CFormLabel for="Resolucion">Resoluci&oacute;n No.</CFormLabel>
-                        <CFormInput id="Resolucion" required />
+                        <CFormInput id="Resolucion"  />
                       </CCol>
                       <CCol :md="6">
                         <CFormLabel for="fechaResolucion">Fecha Resoluci&oacute;n</CFormLabel>
@@ -80,7 +80,13 @@
                       </CCol>
                       <CCol :md="12">
                         <CFormLabel for="conceptoGastoId">Por Concepto de</CFormLabel>
-                        <CFormInput v-model="postRegistroGasto.conceptoGastoId" id="conceptoGastoId" required />
+                      
+                        <CFormSelect required v-model="postRegistroGasto.conceptoGastoId">
+                          <option :key="0">Selecionar concepto</option>
+                          <option v-for="concepto in conceptosGasto " :value="`${concepto.id}`" :key="concepto.id">
+                            {{ concepto.descripcion }}
+                          </option>
+                        </CFormSelect>
                       </CCol>
                       <CCol :md="12">
                         <CFormLabel for="nombre">Cuenta de banco</CFormLabel>
@@ -155,10 +161,12 @@ export default {
 
   data: function () {
     return {
+      onlyNumber,
       cuentasBanco: [],
       isFormEventTypeValidated: false,
       detalleGasto: [],
       cuentaBanco: '',
+      conceptosGasto:[],
       displayBeneficiario: null,
       showBeneficiarioModal: false,
       ShowCodificacionGastoModal: false,
@@ -170,7 +178,7 @@ export default {
         fecha: null,
         etapa: 'Devengado',
         beneficiarioId: null,
-        conceptoGastoId: 5,
+        conceptoGastoId: null,
         bancoId: null,
         numeroCheque: "1",
         totalBruto: 0,
@@ -222,7 +230,13 @@ export default {
       CuentaService.getCuentasDeBancos().then(response => {
         this.cuentasBanco = response.data.data
       })
+      Api.getConceptoGastos().then(response => {
+        this.conceptosGasto = response.data.data
+      })
+       
     },
+
+  
 
 
 
