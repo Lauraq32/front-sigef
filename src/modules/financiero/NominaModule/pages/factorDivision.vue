844f0bfa8630879ddfe417ccc5dce1bcdbe323c3
LIGAMD\avalenzuela <alfredoelian214@gmail.com>
2023-07-28 14:34:57 -0400

    fix code

--- src/modules/financiero/NominaModule/pages/factorDivision.vue
+++ src/modules/financiero/NominaModule/pages/factorDivision.vue
@@ -1,147 +1,153 @@
 <template>
-    <CCardBody>
-        <CCardTitle class="mx-5 pb-4 d-flex justify-content-center">
-            <h6>Escala de Sueldos para Calculo del Impuesto Sobre la Renta</h6>
-        </CCardTitle>
-        <CForm>
-            <div class="row">
-                <div class="col-md-5">
-                    <CFormLabel for="excento">Excento Hasta:</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-5">
-                    <CFormLabel for="excento">Sueldo Escala 1:</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
+    <CCardTitle class="mx-5 pb-4 d-flex justify-content-center">
+        <h5>Escala de Sueldos para Calculo del Impuesto Sobre la Renta</h5>
+    </CCardTitle>
 
-                <div class="input-group col-2">
-                    <span class="input-group-text" id="basic-addon1">%</span>
-                    <input type="text" class="form-control" />
-                </div>
+    <CForm>
+        <div class="container">
+            <div class="d-flex justify-content-center ">
+                <div>
+                    <div class="row">
+                        <div class="col-md-5">
+                            <CFormLabel for="excento">Excento Hasta:</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-5">
+                            <CFormLabel for="excento">Sueldo Escala 1:</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
 
-                <div class="col-5">
-                    <CFormLabel for="excento">Valor Fijo</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-            </div>
+                        <div class="input-group col-2">
+                            <span class="input-group-text" id="basic-addon1">%</span>
+                            <input type="text" class="form-control" />
+                        </div>
 
-            <div class="row">
-                <div class="col-5">
-                    <CFormLabel for="excento">Sueldo Escala 2:</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-                <div class="input-group col-2">
-                    <span class="input-group-text" id="basic-addon1">%</span>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="col-5 input-position">
-                    <CFormInput id="excento" />
-                </div>
-            </div>
+                        <div class="col-5">
+                            <CFormLabel for="excento">Valor Fijo</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
+                    </div>
 
-            <div class="row">
-                <div class="col-5">
-                    <CFormLabel for="excento">Sueldo Escala 3:</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-                <div class="input-group col-2">
-                    <span class="input-group-text" id="basic-addon1">%</span>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="col-5 input-position">
-                    <CFormInput id="excento" />
-                </div>
-            </div>
+                    <div class="row">
+                        <div class="col-5">
+                            <CFormLabel for="excento">Sueldo Escala 2:</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
+                        <div class="input-group col-2">
+                            <span class="input-group-text" id="basic-addon1">%</span>
+                            <input type="text" class="form-control" />
+                        </div>
+                        <div class="col-5 input-position">
+                            <CFormInput id="excento" />
+                        </div>
+                    </div>
 
-            <div class="row">
-                <div class="col-5">
-                    <CFormLabel for="excento">Sueldo Escala 4:</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-                <div class="input-group col-2">
-                    <span class="input-group-text" id="basic-addon1">%</span>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="col-5 input-position">
-                    <CFormInput id="excento" />
-                </div>
-            </div>
+                    <div class="row">
+                        <div class="col-5">
+                            <CFormLabel for="excento">Sueldo Escala 3:</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
+                        <div class="input-group col-2">
+                            <span class="input-group-text" id="basic-addon1">%</span>
+                            <input type="text" class="form-control" />
+                        </div>
+                        <div class="col-5 input-position">
+                            <CFormInput id="excento" />
+                        </div>
+                    </div>
 
-            <div class="row mt-3">
-                <div class="col-6">
-                    <CFormLabel for="excento">Retencion AFP</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-                <div class="col-6">
-                    <CFormLabel for="excento">Hasta el sueldo</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-            </div>
+                    <div class="row">
+                        <div class="col-5">
+                            <CFormLabel for="excento">Sueldo Escala 4:</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
+                        <div class="input-group col-2">
+                            <span class="input-group-text" id="basic-addon1">%</span>
+                            <input type="text" class="form-control" />
+                        </div>
+                        <div class="col-5 input-position">
+                            <CFormInput id="excento" />
+                        </div>
+                    </div>
 
-            <div class="row">
-                <div class="col-6">
-                    <CFormLabel for="excento">Retencion ARS</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-                <div class="col-6">
-                    <CFormLabel for="excento">Hasta el sueldo</CFormLabel>
-                    <CFormInput id="excento" />
-                </div>
-            </div>
-
-            <div class="row fw-bold my-5 mx-3 p-2">
-                <div class="col-7">
                     <div class="row mt-3">
-                        <div class="col-auto">
-                            <CFormLabel for="sueldoMensual" class="col-form-label">Sueldo Mensual:</CFormLabel>
+                        <div class="col-6">
+                            <CFormLabel for="excento">Retencion AFP</CFormLabel>
+                            <CFormInput id="excento" />
                         </div>
-                        <div class="col-auto">
-                            <CFormInput id="sueldoMensual" />
+                        <div class="col-6">
+                            <CFormLabel for="excento">Hasta el sueldo</CFormLabel>
+                            <CFormInput id="excento" />
                         </div>
                     </div>
 
-                    <div class="mt-3">
-                        <CFormLabel for="excento">Sueldo Quincenal:</CFormLabel>
+                    <div class="row">
+                        <div class="col-6">
+                            <CFormLabel for="excento">Retencion ARS</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
+                        <div class="col-6">
+                            <CFormLabel for="excento">Hasta el sueldo</CFormLabel>
+                            <CFormInput id="excento" />
+                        </div>
                     </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">Sueldo Anual:</CFormLabel>
-                    </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">AFP Anual:</CFormLabel>
-                    </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">AFP Mensual:</CFormLabel>
-                    </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">ARS Anual:</CFormLabel>
-                    </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">ARS Mensual:</CFormLabel>
-                    </div>
-                </div>
-                <div class="col-5">
-                    <div class="mt-3">
-                        <CFormLabel for="excento">Sueldo - AFP - ARS:</CFormLabel>
-                    </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">ISR Anual:</CFormLabel>
-                    </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">IRS Mensual:</CFormLabel>
-                    </div>
-                    <div class="mt-3">
-                        <CFormLabel for="excento">IRS Quincenal:</CFormLabel>
+
+                    <div class="row fw-bold my-5 mx-3 p-2">
+                        <div class="col-7">
+                            <div class="row mt-3">
+                                <div class="col-auto">
+                                    <CFormLabel for="sueldoMensual" class="col-form-label">Sueldo Mensual:</CFormLabel>
+                                </div>
+                                <div class="col-auto">
+                                    <CFormInput id="sueldoMensual" />
+                                </div>
+                            </div>
+
+                            <div class="mt-3">
+                                <CFormLabel for="excento">Sueldo Quincenal:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">Sueldo Anual:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">AFP Anual:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">AFP Mensual:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">ARS Anual:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">ARS Mensual:</CFormLabel>
+                            </div>
+                        </div>
+                        <div class="col-5">
+                            <div class="mt-3">
+                                <CFormLabel for="excento">Sueldo - AFP - ARS:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">ISR Anual:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">IRS Mensual:</CFormLabel>
+                            </div>
+                            <div class="mt-3">
+                                <CFormLabel for="excento">IRS Quincenal:</CFormLabel>
+                            </div>
+                        </div>
+                        <div class="mt-3 w-100 d-flex justify-content-end">
+                            <div>
+                                <CButton color="info">Guardar</CButton>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </CForm>
-    </CCardBody>
-    <CFooter>
-        <CButton color="secondary" @click="closeModal">Cerrar</CButton>
-        <CButton color="primary">Guardar</CButton>
-    </CFooter>
+        </div>
+    </CForm>
 </template>
   
 <script>
