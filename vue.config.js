1437212d500688860162b632527ea860b78fd72c
Johanny S. Novas <johanny.novas@lmd@gob.do>
2023-08-13 11:40:06 -0400

    BLD-2(fix): fixing public path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	lintOnSave: false,
+	publicPath: `/${process.env.BASE_URL}`,
 	// devServer: {
 	//   proxy: `${process.env.APP_API_URL ?? 'http://localhost'}`,
 	// },
