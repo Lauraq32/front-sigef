7ba2267adae5dc30d52893063632d0ebd2cae307
Geraldsonp <Geraldson.a.P@gmail.com>
2023-08-18 21:30:50 -0400

    Initial

--- src/modules/financiero/ConciliacionBancaria/components/ChequesNomina.vue
+++ src/modules/financiero/ConciliacionBancaria/components/ChequesNomina.vue
@@ -1,31 +1,104 @@
 <template>
-    <CRow>
-        <CCol sm="12">
-            <h1>Cheques de Nómina</h1>
-        </CCol>
-    </CRow>
-    <CRow>
-        <CCol sm="4">
-            <CButton color="primary">Nuevo</CButton>
-        </CCol>
-        <CCol sm="8" class="selectContainer">
-            <label for="">Mes</label>
-            <select>
-                <option v-for="mes in meses" :key="mes.id" :value="mes.id">{{ mes.nombre }}</option>
-            </select>
-            <label for="">Año</label>
-            <select>
-                <option v-for="year in getYears()" :key="year" :value="year">{{ year }}</option>
-            </select>
-        </CCol>
-    </CRow>
+    <div class="card p-3">
+        <CRow>
+            <CCol sm="12">
+                <h1>Cheques de Nómina</h1>
+            </CCol>
+        </CRow>
+        <hr>
+        <CRow>
+            <CCol sm="4">
+                <CButton color="primary">Cambiar Cuenta</CButton>
+            </CCol>
+            <CCol sm="8" class="selectContainer">
+                <label for="">Mes</label>
+                <select>
+                    <option v-for="mes in meses" :key="mes.id" :value="mes.id">{{ mes.nombre }}</option>
+                </select>
+                <label for="">Año</label>
+                <select>
+                    <option v-for="year in getYears()" :key="year" :value="year">{{ year }}</option>
+                </select>
+            </CCol>
+        </CRow>
+         <hr>
+         <CRow>
+            <CCol sm="12">
+                <CSmartTable
+                    :tableProps="{
+                        striped: true,
+                        hover: true,
+                    }"
+                    :columns="fields"
+                    :items="items"
+                    columnFilter
+                    clickableRows
+                    columnSorter
+                    pagination
+                    :itemsPerPage="5">
+                    <template #programa_codigo={item}>
+                        <td>
+                            {{ item.programa.codigo }}
+                        </td>
+                         <td>
+                            {{ item.programa.nombre }}
+                        </td>
+                    </template>
+                    <template #departamento_codigo={item}>
+                        <td>
+                            {{ item.departamento.codigo }}
+                        </td>
+                        <td>
+                            {{ item.departamento.nombre }}
+                        </td>
+                    </template>
+                </CSmartTable>
+            </CCol>
+            <CCol sm="12">
+                <CButton color="primary">Consultar Cheques Nomina Seleccionada</CButton>
+            </CCol>
+         </CRow>
+    </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import { CSmartTable } from '@coreui/vue-pro'
+import {api} from '@/services/api'
 
 const year = ref({});
 const month = ref({});
+
+const fields = ref([
+    {key: 'fecha', label: 'Fecha'},
+    {group:"Programa", 
+        children: [
+        {key: 'programa_codigo', label: 'Codigo'},
+        {key: 'programa.nombre', label: 'Nombre'},
+        ]
+    },
+    {group:"Departamento", 
+        children: [
+        {key: 'departamento_codigo', label: 'codigo'},
+        {key: 'nombre', label: 'Nombre'},
+        ]
+    },
+    {key: 'fechaCheque', label: 'Fecha Cheque'},
+    {key: 'cantidadCheques', label: 'Cantidad Cheques'},
+    {group:"Secuenciales", 
+        children: [
+        {key: 'inicial', label: 'Inicial'},
+        {key: 'final', label: 'Final'},
+        ]
+    },
+])
+
+const items = ref([
+    { fecha: '01/01/2021', programa:{codigo: '123', nombre: 'Juan Perez'}, departamento:{codigo: '123456', nombre: 'Juan Perez'}, fechaCheque: '01/01/2021', cantidadCheques: 9, secuenciales:{inicial: 2, final: 4} },
+    {fecha: '01/01/2021', programa:{codigo: '123456', nombre: 'Juan Perez'}, departamento:{codigo: '123456', nombre: 'Juan Perez'}, fechaCheque: '01/01/2021', cantidadCheques: 9, secuenciales:{inicial: 2, final: 4} },
+    ///give me more data with the same structure
+    {fecha: '01/01/2021', programa:{codigo: '123456', nombre: 'Juan Perez'}, departamento:{codigo: '123456', nombre: 'Juan Perez'}, fechaCheque: '01/01/2021', cantidadCheques: 9, secuenciales:{inicial: 2, final: 4} },
+]);
 
 const meses = ref([
     { id: 1, nombre: 'Enero' },
@@ -41,7 +114,6 @@ const meses = ref([
     { id: 11, nombre: 'Noviembre' },
     { id: 12, nombre: 'Diciembre' },
 ]);
-
 //create computed that calculates 5 year range
 const getYears = () => {
     const currentYear = new Date().getFullYear();
@@ -51,6 +123,10 @@ const getYears = () => {
     }
     return years;
 };
+
+onMounted(() => {
+    
+});
 
 </script>
 
